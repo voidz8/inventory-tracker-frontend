f1ddb6e6426d7c1544b5763746736358d540f56a
voidz8 <renzovermue@hotmail.com>
2021-07-11 21:02:42 +0200

    Added charts and datasorting

---
+++ src/components/Chart.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Area,
+  AreaChart,
+  Label,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+function Chart({ data, firstDataKey, secondDataKey }) {
+  return (
+    <ResponsiveContainer width={"95%"} height={250}>
+      <AreaChart
+        data={data}
+        margin={{ top: 20, right: 0, left: 20, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id={"sold"} x1="0" y1="0" x2="0" y2="1">
+            <stop offset={"5%"} stopColor={"#2dff19"} stopOpacity={0.8} />
+            <stop offset={"95%"} stopColor={"#2dff19"} stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id={"bought"} x1="0" y1="0" x2="0" y2="1">
+            <stop offset={"5%"} stopColor={"#ff1919"} stopOpacity={0.8} />
+            <stop offset={"95%"} stopColor={"#ff1919"} stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey={"date"} />
+        <YAxis />
+
+        <Tooltip />
+        <Legend />
+        <Area
+          type={"monotone"}
+          stroke={"#ff1919"}
+          dataKey={firstDataKey}
+          fill={"url(#bought)"}
+        />
+        <Area
+          type={"monotone"}
+          stroke={"#2dff19"}
+          dataKey={secondDataKey}
+          fill={"url(#sold)"}
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default Chart;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,47 +2,68 @@ import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import TopMenu from "../components/TopMenu";
 import authContext from "../contexts/AuthContext";
-import {
-  XAxis,
-  YAxis,
-  Tooltip,
-  LineChart,
-  Line,
-  Legend,
-  ResponsiveContainer,
-  Area,
-  AreaChart,
-} from "recharts";
 import axios from "axios";
+import Chart from "../components/Chart";
+import "./HomePage.css";
 
 function HomePage() {
   const authData = useContext(authContext);
   const history = useHistory();
-  const [sneakerData, setSneakerData] = useState();
+  const [sneakerData, setSneakerData] = useState([]);
+  const [itemData, setItemData] = useState([]);
+  const [botData, setBotData] = useState([]);
+  const url = "http://localhost:8080/";
+  const header = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  };
 
   useEffect(async () => {
-    const response = await axios.get("http://localhost:8080/sneakers/data", {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-    setSneakerData(response.data);
-    console.log(response.data);
+    const sneakerResponse = await axios.get(url + "sneakers/data", header);
+    const itemResponse = await axios.get(url + "items/data", header);
+    const botResponse = await axios.get(url + "bots/data", header);
+
+    setSneakerData(sneakerResponse.data.sort(sortByDate));
+    setItemData(itemResponse.data.sort(sortByDate));
+    setBotData(botResponse.data.sort(sortByDate));
   }, []);
+
+  function sortByDate(a, b) {
+    let dateA = new Date(a.date),
+      dateB = new Date(b.date);
+    return dateA - dateB;
+  }
 
   return (
     <div className={"HomePage"}>
       <TopMenu />
-      <ResponsiveContainer>
-        <AreaChart data={sneakerData}>
-          <XAxis dataKey={"date"} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Area dataKey={"priceBought"} />
-          <Area dataKey={"priceSold"} />
-        </AreaChart>
-      </ResponsiveContainer>
+      <div className={"charts-container"}>
+        <div className={"chart"}>
+          <label className={"chart-name"}>Sneaker Overview</label>
+          <Chart
+            data={sneakerData}
+            firstDataKey={"priceBought"}
+            secondDataKey={"priceSold"}
+          />
+        </div>
+        <div className={"chart"}>
+          <label className={"chart-name"}>Item Overview</label>
+          <Chart
+            data={itemData}
+            firstDataKey={"priceBought"}
+            secondDataKey={"priceSold"}
+          />
+        </div>
+        <div className={"chart"}>
+          <label className={"chart-name"}>Bot Overview</label>
+          <Chart
+            data={botData}
+            firstDataKey={"priceBought"}
+            secondDataKey={"priceSold"}
+          />
+        </div>
+      </div>
     </div>
   );
 }
